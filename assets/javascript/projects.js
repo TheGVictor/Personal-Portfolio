e2be77e4de777b9fe594f08f499fadb4bb8385e7
TheGVictor <gabrielsancinetti@gmail.com>
2024-04-13 11:44:43 -0300

    Modal project finalized

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -2,5 +2,8 @@ let projectsJson = [
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
+    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
+    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
+    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'}
 ]
