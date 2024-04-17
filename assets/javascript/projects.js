efee1bcf4fe83c2d13d80b018b3246c2c9120d03
TheGVictor <gabrielsancinetti@gmail.com>
2024-04-17 14:23:34 -0300

    Add library ScrollReveal

--- assets/javascript/projects.js
+++ assets/javascript/projects.js
@@ -1,9 +1,4 @@
 let projectsJson = [
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
-    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
-    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
-    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
-    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
-    {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'},
     {id: 1, name: 'CEP Founder', description: 'Um site para localizar o endereço, de acordo com o CEP'}
 ]
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,17 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.profileDesk')
+sr.reveal('.carreerDesk')
+sr.reveal('.projectsDesk')
+
+sr.reveal('.textTitle', { scale: 0.85 })
+
+sr.reveal('.colored', {
+    rotate: {x: 0, y: 80, z:0},
+    delay: 250
+})
+
+
 projectsJson.map((item, key)=>{
     let project = document.querySelector('.modal .project').cloneNode(true)
     document.querySelector('.projectsBoard').append( project )
